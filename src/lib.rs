0cb57d85078df04b8c7c544857ad18340859d2d9
im97mori <github@im97mori.org>
2023-08-27 13:50:59 +0900

    add Complete List of 128-bit Service Class UUIDs

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,13 @@ pub mod data_types {
     pub mod appearance;
     pub mod big_info;
     pub mod channel_map_update_indication;
+    pub mod complete_list_of_128bit_service_uuids;
     pub mod complete_list_of_16bit_service_uuids;
     pub mod complete_list_of_32bit_service_uuids;
     pub mod complete_local_name;
     pub mod data_type;
 }
 
-pub use data_types::advertising_interval::AdvertisingInterval;
-pub use data_types::advertising_interval_long::AdvertisingIntervalLong;
-pub use data_types::appearance::Appearance;
-pub use data_types::big_info::BigInfo;
-pub use data_types::channel_map_update_indication::ChannelMapUpdateIndication;
-pub use data_types::complete_list_of_16bit_service_uuids::CompleteListOf16BitServiceUuids;
-pub use data_types::complete_list_of_32bit_service_uuids::CompleteListOf32BitServiceUuids;
-pub use data_types::complete_local_name::CompleteLocalName;
-pub use data_types::data_type::DataType;
 use uuid::{uuid, Uuid};
 
 /// BASE UUID.
